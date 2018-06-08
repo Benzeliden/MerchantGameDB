1056ed52a5d40a65f7bb186e78e2ff451425455b
Benzeliden <denis.krasakov@gmail.com>
2018-06-08 18:37:40 +0300

    Fixing error, removing old guides system

--- js/controllers/bodyCtrl.js
+++ js/controllers/bodyCtrl.js
@@ -1,16 +1,8 @@
 // Controller for the <body>
 // Used for anything outside of the control of other controllers
 angular.module('mainApp')
-	.controller('bodyCtrl', function($scope, guidesService) {
-
+	.controller('bodyCtrl', function($scope) {
 		// Game Version Numbers
 		$scope.betaVersion = "2.461";
 		$scope.liveVersion = "2.47";
-
-		// Logic for the "Latest Guides" sidebar widget
-		guidesService.getGuides().then(function (listOfGuides) {
-			$scope.guidesList = listOfGuides;
-		})
-		$scope.getAuthByID = guidesService.getAuthorByID;
-
 	})
